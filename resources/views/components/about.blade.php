9854fc03b5e2f512e7596fc65463038e1b13a14d
AbeNalee <abrahamaguvasu@gmail.com>
2021-08-19 19:33:43 +0300

    added information

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -12,52 +12,64 @@
                         <div class="accordion" id="accordionExample">
                             <div class="card">
                                 <div class="card-header" id="headingOne">
-                                    <a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Frequently Asked Question One</a>
+                                    <a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">How do you handle revision</a>
                                 </div>
 
                                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
                                     <div class="card-body">
-                                        <p class="text">Morbi vehicula arcu et pellentesque tincidunt. Nunc ligula nulla, lobortis a elementum non, vulputate ut arcu. Aliquam erat volutpat. Nullam lacinia felis.</p>
+                                        <p class="text">
+                                            We don't charge for revision when our writer has made a mistake; however, a small fee will be charged if you provide additional instructions. Therefore, provide detailed instructions to facilitate an easier delivery
+                                        </p>
                                     </div>
                                 </div>
                             </div> <!-- card -->
                             <div class="card">
                                 <div class="card-header" id="headingTwo">
-                                    <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Frequently Asked Question Two</a>
+                                    <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Do you give discounts on bulk orders?</a>
                                 </div>
                                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                                     <div class="card-body">
-                                        <p class="text">Morbi vehicula arcu et pellentesque tincidunt. Nunc ligula nulla, lobortis a elementum non, vulputate ut arcu. Aliquam erat volutpat. Nullam lacinia felis.</p>
+                                        <p class="text">Yes, we give quantity discounts when you have a bulk order.</p>
                                     </div>
                                 </div>
                             </div> <!-- card -->
                             <div class="card">
                                 <div class="card-header" id="headingThree">
-                                    <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Frequently Asked Question Three</a>
+                                    <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">What is your turnaround time?</a>
                                 </div>
                                 <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
                                     <div class="card-body">
-                                        <p class="text">Morbi vehicula arcu et pellentesque tincidunt. Nunc ligula nulla, lobortis a elementum non, vulputate ut arcu. Aliquam erat volutpat. Nullam lacinia felis.</p>
+                                        <p class="text">The time for submitting a project depends on its complexity. If you need it urgently, we charge extra for the project</p>
                                     </div>
                                 </div>
                             </div> <!-- card -->
                             <div class="card">
                                 <div class="card-header" id="headingFore">
-                                    <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseFore" aria-expanded="false" aria-controls="collapseFore">Frequently Asked Question Four</a>
+                                    <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseFore" aria-expanded="false" aria-controls="collapseFore">What tools do you use?</a>
                                 </div>
                                 <div id="collapseFore" class="collapse" aria-labelledby="headingFore" data-parent="#accordionExample">
                                     <div class="card-body">
-                                        <p class="text">Morbi vehicula arcu et pellentesque tincidunt. Nunc ligula nulla, lobortis a elementum non, vulputate ut arcu. Aliquam erat volutpat. Nullam lacinia felis.</p>
+                                        <p class="text">We use Grammarly to check for errors and edit the work. We use SPSS for data analysis and check plagiarism through Turnitin before submitting. Plagiarism reports can be shared when needed.</p>
                                     </div>
                                 </div>
                             </div> <!-- card -->
                             <div class="card">
                                 <div class="card-header" id="headingFive">
-                                    <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">Frequently Asked Question Five</a>
+                                    <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">Do you have a refund policy?</a>
                                 </div>
                                 <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
                                     <div class="card-body">
-                                        <p class="text">Morbi vehicula arcu et pellentesque tincidunt. Nunc ligula nulla, lobortis a elementum non, vulputate ut arcu. Aliquam erat volutpat. Nullam lacinia felis.</p>
+                                        <p class="text">We have a money-back guarantee policy if your order does not meet the guidelines you provided. Our quality assurance team checks the order with the instructions. A refund is offered when; the work has been entirely plagiarized, the order is incomplete, and the order was submitted after the deadline. The refund is processed within two days after the request for a refund</p>
+                                    </div>
+                                </div>
+                            </div> <!-- card -->
+                            <div class="card">
+                                <div class="card-header" id="headingSix">
+                                    <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">Can you write in all citation formats?</a>
+                                </div>
+                                <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionExample">
+                                    <div class="card-body">
+                                        <p class="text">We deliver content in all citation styles and formats by sticking to the instructions given.</p>
                                     </div>
                                 </div>
                             </div> <!-- card -->
@@ -65,9 +77,12 @@
                     </div> <!-- faq accordion -->
                 </div> <!-- faq content -->
             </div>
-            <div class="col-lg-7">
+            <div class="col-lg-7 d-flex bg-image align-items-center" >
+{{--                style="background-image: url({{ asset('/assets/images/logo-2.jpg') }}); opacity: 0.5"--}}
                 <div class="about-image mt-50">
-                    <img src="assets/images/about.jpg" alt="about">
+                    <p class="text font-weight-bolder">
+                        We've been working as professional writers in the academic writing market for the past nine years, and we've always provided high-quality work to help our clients achieve their goals. We cover a broad range of academic disciplines at all educational levels and adhere to strict deadlines. Our primary objective is to maintain a secure relationship with our clients by not disclosing their private information to the public. Contact us today for well-presented, high-quality work that can help you improve your grades
+                    </p>
                 </div> <!-- faq image -->
             </div>
         </div> <!-- row -->
--- resources/views/components/services.blade.php
+++ resources/views/components/services.blade.php
@@ -14,45 +14,101 @@
             <div class="col-lg-4 col-md-7 col-sm-9">
                 <div class="single-features mt-40">
                     <div class="features-title-icon d-flex justify-content-between">
-                        <h4 class="features-title"><a href="#">Essay Writing</a></h4>
+                        <h4 class="features-title"><a href="#">Essays</a></h4>
                         <div class="features-icon">
-                            <i class="lni lni-brush"></i>
+                            <i class="lni lni-notepad"></i>
                             <img class="shape" src="assets/images/f-shape-1.svg" alt="Shape">
                         </div>
                     </div>
                     <div class="features-content">
-                        <p class="text">Short description for the ones who look for something new. Short description for the ones who look for something new.</p>
-                        <a class="features-btn" href="#">LEARN MORE</a>
+                        <p class="text">We deliver the best composition of essays by sticking to the research topic. The essay will be unique and delivered in the appropriate format.</p>
+{{--                        <a class="features-btn" href="#">LEARN MORE</a>--}}
                     </div>
                 </div> <!-- single features -->
             </div>
             <div class="col-lg-4 col-md-7 col-sm-9">
                 <div class="single-features mt-40">
                     <div class="features-title-icon d-flex justify-content-between">
-                        <h4 class="features-title"><a href="#">Article writing</a></h4>
+                        <h4 class="features-title"><a href="#">Term Papers</a></h4>
                         <div class="features-icon">
-                            <i class="lni lni-layout"></i>
+                            <i class="lni lni-book"></i>
                             <img class="shape" src="assets/images/f-shape-1.svg" alt="Shape">
                         </div>
                     </div>
                     <div class="features-content">
-                        <p class="text">Short description for the ones who look for something new. Short description for the ones who look for something new.</p>
-                        <a class="features-btn" href="#">LEARN MORE</a>
+                        <p class="text">We write your term paper from scratch by focusing on the topic in detail. Your term paper will reflect on the length and scope of your research topic and be conveyed at your time's convenience.</p>
+{{--                        <a class="features-btn" href="#">LEARN MORE</a>--}}
                     </div>
                 </div> <!-- single features -->
             </div>
             <div class="col-lg-4 col-md-7 col-sm-9">
                 <div class="single-features mt-40">
                     <div class="features-title-icon d-flex justify-content-between">
-                        <h4 class="features-title"><a href="#">Other Service</a></h4>
+                        <h4 class="features-title"><a href="#">Case studies</a></h4>
+                        <div class="features-icon">
+                            <i class="lni lni-magnifier"></i>
+                            <img class="shape" src="assets/images/f-shape-1.svg" alt="Shape">
+                        </div>
+                    </div>
+                    <div class="features-content">
+                        <p class="text">Our team examines a specific research problem to find the case study through critical analysis of the research design.</p>
+{{--                        <a class="features-btn" href="#">LEARN MORE</a>--}}
+                    </div>
+                </div> <!-- single features -->
+            </div>
+            <div class="col-lg-4 col-md-7 col-sm-9">
+                <div class="single-features mt-40">
+                    <div class="features-title-icon d-flex justify-content-between">
+                        <h4 class="features-title"><a href="#">Dissertations</a></h4>
+                        <div class="features-icon">
+                            <i class="lni lni-search"></i>
+                            <img class="shape" src="assets/images/f-shape-1.svg" alt="Shape">
+                        </div>
+                    </div>
+                    <div class="features-content">
+                        <p class="text">We deliver in-depth research on the specific topic by answering the research question with a conclusive outcome.</p>
+{{--                        <a class="features-btn" href="#">LEARN MORE</a>--}}
+                    </div>
+                </div> <!-- single features -->
+            </div>
+            <div class="col-lg-4 col-md-7 col-sm-9">
+                <div class="single-features mt-40">
+                    <div class="features-title-icon d-flex justify-content-between">
+                        <h4 class="features-title"><a href="#">Research papers</a></h4>
+                        <div class="features-icon">
+                            <i class="lni lni-magnifier"></i>
+                            <img class="shape" src="assets/images/f-shape-1.svg" alt="Shape">
+                        </div>
+                    </div>
+                    <div class="features-content">
+                        <p class="text">Our team analyses and interprets the research topic by coming up with the best argument to give the best results.</p>
+{{--                        <a class="features-btn" href="#">LEARN MORE</a>--}}
+                    </div>
+                </div> <!-- single features -->
+            </div>
+            <div class="col-lg-4 col-md-7 col-sm-9">
+                <div class="single-features mt-40">
+                    <div class="features-title-icon d-flex justify-content-between">
+                        <h4 class="features-title"><a href="#">Others</a></h4>
                         <div class="features-icon">
                             <i class="lni lni-bolt"></i>
                             <img class="shape" src="assets/images/f-shape-1.svg" alt="Shape">
                         </div>
                     </div>
                     <div class="features-content">
-                        <p class="text">Short description for the ones who look for something new. Short description for the ones who look for something new.</p>
-                        <a class="features-btn" href="#">LEARN MORE</a>
+                        <ul>
+                            <li>
+                                <p class="text">
+                                    Homework
+                                </p>
+                            </li>
+                            <li>
+                                <p class="text">
+                                    Lab reports
+                                </p>
+                            </li>
+                        </ul>
+{{--                        <a class="features-btn" href="#">LEARN MORE</a>--}}
                     </div>
                 </div> <!-- single features -->
             </div>
