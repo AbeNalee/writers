2a9082086f590ffa0fe531a51add8f879e84db81
AbeNalee <abrahamaguvasu@gmail.com>
2021-08-27 18:20:57 +0300

    added configs

---
+++ config/writers.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Return all the configs related to this project
+ */
+return [
+    'service-tags' => [
+        'Research Papers', 'Essays', 'Term Papers', 'Case Studies', 'Dissertations', 'Others'
+    ],
+];
