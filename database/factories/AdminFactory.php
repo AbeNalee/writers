67d19add645ba88676edca9062ec542f6a115010
AbeNalee <abrahamaguvasu@gmail.com>
2021-09-02 00:19:03 +0300

    seeder

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class AdminFactory extends Factory
@@ -22,11 +23,12 @@ class AdminFactory extends Factory
      */
     public function definition()
     {
+        $password = Hash::make('2020!Gulorian');
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => 'Tim',
+            'email' => 'tim@classifiedwriters.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => $password,
             'remember_token' => Str::random(10),
         ];
     }
