884443d9cd5019bc74b6ad47570c8eb4e31cf2f8
AbeNalee <abrahamaguvasu@gmail.com>
2021-09-13 15:40:26 +0300

    mail

--- config/mail.php
+++ config/mail.php
@@ -43,11 +43,11 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
-            'ssl' => [
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-                'allow_self_signed' => true,
-            ],
+//            'ssl' => [
+//                'verify_peer' => false,
+//                'verify_peer_name' => false,
+//                'allow_self_signed' => true,
+//            ],
         ],
 
         'ses' => [
